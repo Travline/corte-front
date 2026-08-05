c8dc273e8c9cb7f88112ad8c0379bc81892cf5d2
Travline <poluxmorales1@gmail.com>
2026-08-05 03:42:56 -0500

    trying responsive mobile first

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,7 @@
 export const Home = () => {
   return (
     <div className="bg-grid p-5 min-h-dvh flex flex-column justify-center items-center">
-      <h1 className="text-center text-rat text-4xl font-bold">Bienvenido a Corte</h1>
+      <h1 className="text-center text-rat text-2xl lg:text-4xl font-bold">Bienvenido a Corte</h1>
     </div>
   )
 }
